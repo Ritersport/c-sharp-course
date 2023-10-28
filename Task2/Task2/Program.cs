28fb10cca01939fbd7a22f66f3dbeae6a61ed323
Ritersport <leomkrita2001@gmail.com>
2023-10-28 12:02:04 +0700

    missed files added

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using Competitor;
+using Experiment;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shuffler;
+using Strategy;
+
+namespace Task2;
+
+class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args)
+            .Build()
+            .Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<CollisiumExperimentWorker>();
+                services.AddScoped<Sandbox>();
+                services.AddScoped<IDeckShuffler, DeckShuffler>();
+                services.AddScoped<ICompetitor>(x => new Competitor.Competitor(new StrategyAlwaysFirst()));
+                services.AddScoped<ICompetitor>(x => new Competitor.Competitor(new StrategyAlwaysFirst()));
+
+            });
+    }
+}
